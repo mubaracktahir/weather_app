e6a0de907f0823f8422402843e6f08c40bb9ea98
ENIKE BRAIMOH <richardbraimoh@gmail.com>
2021-06-21 22:56:45 +0100

    added a base fragment

--- app/src/main/java/com/enike/weatherapp/MainActivity.kt
+++ app/src/main/java/com/enike/weatherapp/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.enike.weatherapp
 
+import android.graphics.Color
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
+import androidx.annotation.RequiresApi
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setTransparentStatusBar()
+    }
+
+
+    private fun setTransparentStatusBar() {
+        // Hide the status bar.
+        window.decorView.apply {
+            systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+            systemUiVisibility =
+                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_FULLSCREEN
+
+        }
     }
 }
