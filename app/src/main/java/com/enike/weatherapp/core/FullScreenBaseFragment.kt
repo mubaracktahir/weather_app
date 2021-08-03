c49ee0810e4018bd5b9db1d6f2f906a1a136871e
enikebraimoh <richardbraimoh@gmail.com>
2021-08-03 16:08:04 +0100

    done with Home screen

--- app/src/main/java/com/enike/weatherapp/core/FullScreenBaseFragment.kt
+++ app/src/main/java/com/enike/weatherapp/core/FullScreenBaseFragment.kt
@@ -9,12 +9,27 @@ abstract class FullScreenBaseFragment<DB : ViewDataBinding>(@LayoutRes val layou
 
     override fun onResume() {
         super.onResume()
+        setTransparentStatusBar()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        activity?.window?.decorView?.systemUiVisibility = View.SYSTEM_UI_FLAG_VISIBLE
+    }
+
+    private fun setTransparentStatusBar() {
+        // Hide the status bar.
         requireActivity().window.decorView.apply {
             systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
             systemUiVisibility =
                 View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_FULLSCREEN
-
         }
+        activity?.window?.decorView?.systemUiVisibility = View.SYSTEM_UI_FLAG_LOW_PROFILE or
+                View.SYSTEM_UI_FLAG_FULLSCREEN or
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
+                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
     }
 
 }
