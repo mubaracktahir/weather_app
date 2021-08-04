22bff209839035d5c53dcf91c5c3c8adb905ebc0
enikebraimoh <richardbraimoh@gmail.com>
2021-08-05 00:00:24 +0100

    done with search screen

--- app/src/main/java/com/enike/weatherapp/ui/home/HomeFragment.kt
+++ app/src/main/java/com/enike/weatherapp/ui/home/HomeFragment.kt
@@ -5,11 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.enike.weatherapp.R
 import com.enike.weatherapp.core.BaseFragment
 import com.enike.weatherapp.databinding.FragmentHomeBinding
 
 class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
-
+    override fun start() {
+        super.start()
+        binding.searchIcon.setOnClickListener {
+            findNavController().navigate(HomeFragmentDirections.actionHomeFragmentToSearchFragment())
+        }
+    }
 
 }
